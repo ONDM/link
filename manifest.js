829887c0251364c9bc74d3b6fbc6deaa0c273353
OND <149911739+ONDM@users.noreply.github.com>
2024-02-21 21:02:47 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,14 +2,14 @@
   "name": "link",
   "short_name": "link",
   "description": "OND LINK",
-  "start_url": "link/",
+  "start_url": "/link/",
   "display": "standalone",
   "background_color": "#000000",
   "theme_color": "#000000",
   "icons":
   [
     {
-      "src": "link/favicon.png",
+      "src": "/link/favicon.png",
       "sizes": "512x512",
       "type": "image/png"
     }
